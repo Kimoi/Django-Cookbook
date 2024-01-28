9c589bdcc6b7472dd222bbb33da021fd139071c0
Mark_Dolgov <hontonikimoi@gmail.com>
2024-01-28 19:57:51 +0300

    add models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,39 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    usage_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name = "Recipe"
+        verbose_name_plural = "Recipes"
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Product, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    weight = models.PositiveIntegerField(
+        default=100, validators=[MinValueValidator(1), MaxValueValidator(100000)])
+
+    class Meta:
+        verbose_name = "Recipe Ingredient"
+        verbose_name_plural = "Recipe Ingredients"
+
+    def __str__(self):
+        return f'{self.ingredient} from recipe {self.recipe} - {self.weight}g'
